a8e97571f2129b7bdbba56cbf0b47d04affbff97
Michal Furmankiewicz <mifurm@microsoft.com>
2017-11-16 16:56:14 +0100

    update

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,7 +1,9 @@
-﻿import http = require('http');
-import os = require('os');
-var port = process.env.port || 1337
+"use strict";
+var http = require("http");
+var os = require("os");
+var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World app served from instance name ' + os.hostname() + os.platform());
 }).listen(port);
+//# sourceMappingURL=server.js.map
